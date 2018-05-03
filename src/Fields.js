176769775db91fd5e54658053b3b382c048aff1a
XD0M3 <xd0m3@staff.eslgaming.com>
2018-05-03 08:47:42 +0200

    learning react - try some things

---
+++ src/Fields.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Fields extends Component {
+	constructor(props) {
+		super(props);
+		this.count = props.count;
+	}
+
+	render() {
+		return (
+		<h1	
+		);
+	}
+}
+
+export default Fields;
