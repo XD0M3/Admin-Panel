c2d44fe9eddf4c280e7a35250f870f88dfb51f87
XD0M3 <xd0m3@staff.eslgaming.com>
2018-05-04 16:07:18 +0200

    add express

--- src/Content.js
+++ src/Content.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class Title extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {users: []};
     }
 
     componentDidMount() {
-        
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
     }
 
     componentWillUnmount() {
@@ -18,7 +20,9 @@ class Title extends Component {
     render() {
 
         return (
-            <h1>Hallo</h1>
+            <div className="container">
+                {this.state.users.map(user => <h1>{user.id}</h1>)}
+            </div>
         );
     }
 }
